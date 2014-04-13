19f325af288a73c1587e93e652dfe4dd43de75df
Xiaobo Sun <sxbwjm@Xiaobos-MacBook-Air.local>
2014-04-13 16:34:14 -0400

    added plot_script.c

---
+++ init_conditions.c
@@ -0,0 +1,48 @@
+//
+//  InitConditions.c
+//  Project1
+//
+//  Created by Xiaobo Sun on 4/11/14.
+//  Copyright (c) 2014 Xiaobo Sun. All rights reserved.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+/*************************************************************
+ *               functions for inital condition
+ **************************************************************/
+
+// u(x,0) = N(N+1)/cosh(x)^2  N = 0.25
+double initFunc1(double x)
+{
+    double N = 0.25;
+    double tmp = cosh(x - 10);
+    return N * (N + 1) / (tmp * tmp);
+}
+
+// u(x,0) = N(N+1)/cosh(x)^2  N = 1
+double initFunc2(double x)
+{
+    double N = 1;
+    double tmp = cosh(x - 10);
+    return N * (N + 1) / (tmp * tmp);
+}
+
+// one solition: u(x,0)= (c/2)/cosh2(√c / 2)
+double initFunc3(double x)
+{
+    double c = 5 ;
+    double tmp = cosh( sqrt(c) * (-x+20) / 2);
+    return c / (tmp * tmp) / 2;
+}
+
+// two solitions: u(x,0)= (c1/2)/cosh2(√c1 / 2) + (c2/2)/cosh2(√c2 / 2)
+double initFunc4(double x)
+{
+    double c1 = 10;
+    double c2 = 3;
+    double tmp1 = cosh( sqrt(c1) * (x-10) / 2);
+    double tmp2 = cosh( sqrt(c2) * (x-20) / 2);
+    return c1 / (tmp1 * tmp1) / 2 + c2/ (tmp2 * tmp2) / 2;
+}
---
+++ plot_script.c
@@ -0,0 +1,49 @@
+//
+//  plot_script.c
+//  Project1
+//
+//  Created by Xiaobo Sun on 4/12/14.
+//  Copyright (c) 2014 Xiaobo Sun. All rights reserved.
+//
+
+#include <stdio.h>
+#include "common.h"
+
+int createPlotScript(char* fileName, int type)
+{
+    FILE* f = fopen(fileName, "w");
+    if(f == NULL)
+    {
+        return -1;
+    }
+    
+    float yRange = 0.35;
+    switch(type)
+    {
+        case 0:
+            yRange = 0.35;
+            break;
+        case 1:
+            yRange = 2.5;
+            break;
+        case 2:
+            yRange = 3;
+            break;
+        case 3:
+            yRange = 6;
+            break;
+        default:
+            break;
+    }
+    fprintf(f, "if (exist(\"n\")==0 || n<0) n=0\n");
+    fprintf(f, "plot [0:40] [-0.3:%f] sprintf(\"output/data-%%d\", n) with lines", yRange);
+    fprintf(f, " title sprintf(\"time:%%f\", n * %f)\n",  DELTA_T * T_STEPS_PER_FILE);
+    fprintf(f,"if (n<1999) n=n+1; reread\n");
+    fprintf(f,"n=-1\n");
+    
+    fclose(f);
+    
+    return 0;
+    
+    
+}
